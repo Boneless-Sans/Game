9132b7c4ca615fc789583cdfd2e7aecf0dcbad3a
Boneless-Sans <bonelessmail0101@gmail.com>
2024-01-24 14:18:37 -0800

    Working on player and mainmenu framework

--- src/main/java/com/boneless/game/Game.java
+++ src/main/java/com/boneless/game/Game.java
@@ -6,16 +6,19 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.util.Objects;
 
-public class Game extends JFrame {
+public class Game extends JFrame implements KeyListener {
     private boolean debug = false;
     private final String[] debugColors = {"\u001B[30m", "\u001B[31m","\u001B[32m","\u001B[33m","\u001B[34m","\u001B[35m","\u001B[36m","\u001B[37m"};
     //Black - 0, Red - 1, Green - 2, Yellow - 3, Blue - 4, Purple - 5, Cyan - 6, White - 7
-    //Green: General Debugging info, Red: Error
+    //Rule of Thumb: Green: General Debugging info, Red: Error
+    private final Player player = new Player();
     public Game(String inDebug){
         if(Objects.equals(inDebug, "dev")){
             this.debug = true;
@@ -27,15 +30,19 @@ public class Game extends JFrame {
             System.out.println(debugColors[2] + "Debugging Enabled!" + debugColors[7]);
         }
         addWindowListener(adapter());
+        addKeyListener(this);
         setSize(1200,900);
         setLocationRelativeTo(null);
+        add(new MainMenu());
 
-        add(gameBoard());
+//        JPanel gameBoard = gameBoard();
+//        add(gameBoard);
+//        gameBoard.add(player);
 
         setVisible(true);
     }
     private JPanel gameBoard(){
-        JPanel panel = new JPanel(new GridLayout(10,10,2,2));
+        JPanel panel = new JPanel(null);
         panel.setBackground(Color.red);
         return panel;
     }
@@ -56,5 +63,22 @@ public class Game extends JFrame {
                 }
             }
         };
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        if(debug && e.getKeyChar() == 'w'){
+
+        }
+    }
+    @Override
+    public void keyPressed(KeyEvent e) {}
+    @Override
+    public void keyReleased(KeyEvent e){}
+
+    private static class MainMenu extends JPanel{
+        public MainMenu(){
+            setBackground(Color.red);
+        }
     }
 }
--- src/main/java/com/boneless/game/Player.java
+++ src/main/java/com/boneless/game/Player.java
@@ -1,4 +1,12 @@
 package com.boneless.game;
 
-public class Player {
+import javax.swing.*;
+import java.awt.*;
+
+public class Player extends JPanel {
+    private int health;
+    public Player(){
+        setPreferredSize(new Dimension(50,50));
+        setBackground(Color.black);
+    }
 }
