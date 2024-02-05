c261dfa6f5dfb7a57abb35c9c3594704a97ac777
Boneless-Sans <bonelessmail0101@gmail.com>
2024-02-05 15:09:19 -0800

    Very broken :)

--- src/main/java/com/boneless/game/util/MapObject.java
+++ src/main/java/com/boneless/game/util/MapObject.java
@@ -4,14 +4,16 @@ import com.boneless.game.Player;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 public abstract class MapObject extends JPanel {
     protected boolean debug;
     protected String xPos;
     protected String yPos;
     protected String fileName;
-    protected int sizeX;
-    protected int sizeY;
+    protected String sizeX;
+    protected String sizeY;
+    protected String anchor;
     protected JFrame frame;
 
     public MapObject(String fileName, JFrame frame, boolean debug) {
@@ -21,65 +23,82 @@ public abstract class MapObject extends JPanel {
 
         this.xPos = JsonFile.readWithThreeKeys("maps/" + fileName, "objects", getObjectName(), "x");
         this.yPos = JsonFile.readWithThreeKeys("maps/" + fileName, "objects", getObjectName(), "y");
-        this.sizeX = Integer.parseInt(JsonFile.readWithThreeKeys("maps/" + fileName, "objects", getObjectName(), "sizeX"));
-        this.sizeY = Integer.parseInt(JsonFile.readWithThreeKeys("maps/" + fileName, "objects", getObjectName(), "sizeY"));
-        setBackground(getBackgroundColor());
-        if (containsNumbers(xPos) && containsNumbers(yPos)) {
-            setBounds(Integer.parseInt(xPos), Integer.parseInt(yPos), sizeX, sizeY);
-        } else {
-            int newXPos = switch (xPos) {
-                case "l" -> 0;
-                case "r" -> frame.getWidth() - sizeX - 10;
-                case "m" -> (frame.getWidth() / 2) - 40;
-                default -> Integer.parseInt(xPos);
-            };
-            int newYPos = switch (yPos) {
-                case "t" -> 0;
-                case "b" -> frame.getHeight() - sizeY - 35;
-                case "m" -> (frame.getHeight() / 2) - 40;
-                default -> Integer.parseInt(yPos);
-            };
-            setBounds(newXPos, newYPos, sizeX, sizeY);
-        }
+        this.sizeX = JsonFile.readWithThreeKeys("maps/" + fileName, "objects", getObjectName(), "sizeX");
+        this.sizeY = JsonFile.readWithThreeKeys("maps/" + fileName, "objects", getObjectName(), "sizeY");
+        this.anchor = JsonFile.readWithThreeKeys("maps/" + fileName, "objects", getObjectName(), "a");
+
+        int newXPos = getNewX(xPos);
+        int newYPos = getNewY(yPos);
+        int newSizeX = getNewSizeX(sizeX);
+        int newSizeY = getNewSizeY(sizeY);
+        setBounds(newXPos, newYPos, newSizeX, newSizeY);
     }
     public MapObject(String fileName, JFrame frame, boolean debug, int blockNum) {
         this.debug = debug;
         this.frame = frame;
         this.fileName = fileName;
 
-        System.out.println("Block: " + blockNum);
-        System.out.println(fileName);
         this.xPos = JsonFile.readWithFourKeys("maps/" + fileName, "objects", "blocks",getObjectName() + blockNum, "x");
         this.yPos = JsonFile.readWithFourKeys("maps/" + fileName, "objects", "blocks",getObjectName() + blockNum, "y");
-        this.sizeX = Integer.parseInt(JsonFile.readWithFourKeys("maps/" + fileName, "objects", "blocks", getObjectName() + blockNum, "sizeX"));
-        this.sizeY = Integer.parseInt(JsonFile.readWithFourKeys("maps/" + fileName, "objects", "blocks", getObjectName() + blockNum, "sizeY"));
-        setBackground(getBackgroundColor());
-        if (containsNumbers(xPos) && containsNumbers(yPos)) {
-            setBounds(Integer.parseInt(xPos), Integer.parseInt(yPos), sizeX, sizeY);
-        } else {
-            int newXPos = getNewX(xPos);
-            int newYPos = getNewY(yPos);
-            setBounds(newXPos, newYPos, sizeX, sizeY);
-        }
+        this.sizeX = JsonFile.readWithFourKeys("maps/" + fileName, "objects", "blocks", getObjectName() + blockNum, "sizeX");
+        this.sizeY = JsonFile.readWithFourKeys("maps/" + fileName, "objects", "blocks", getObjectName() + blockNum, "sizeY");
+        this.anchor = JsonFile.readWithFourKeys("maps/" + fileName, "objects", "blocks", getObjectName() + blockNum, "a");
+
+        int newXPos = getNewX(xPos);
+        int newYPos = getNewY(yPos);
+        int newSizeX = getNewSizeX(sizeX);
+        int newSizeY = getNewSizeY(sizeY);
+        setBounds(newXPos, newYPos, newSizeX, newSizeY);
+
     }
     private int getNewX(String xPos){
         return switch (xPos) {
             case "l" -> 0;
-            case "r" -> frame.getWidth() - sizeX - 10;
-            case "m" -> (frame.getWidth() / 2) - sizeX;
+            case "r" -> frame.getWidth() - getNewSizeX(sizeX) - 10;
+            case "m" -> (frame.getWidth() / 2) - (getNewSizeX(sizeX) / 2);
             default -> Integer.parseInt(xPos);
         };
     }
     private int getNewY(String yPos){
         return switch (yPos) {
             case "t" -> 0;
-            case "b" -> frame.getHeight() - sizeY - 35;
-            case "m" -> (frame.getHeight() / 2) - sizeY;
+            case "b" -> frame.getHeight() - getNewSizeY(sizeY) - 35;
+            case "m" -> (frame.getHeight() / 2) - (getNewSizeY(sizeY) / 2);
             default -> Integer.parseInt(yPos);
         };
     }
+    private int getNewSizeX(String sizeX){
+        if(Objects.equals(sizeX, "s")){
+            return frame.getWidth();
+        }else if(sizeX != null && sizeX.matches("[0-9]+")){
+            return Integer.parseInt(sizeX);
+        }else{
+            if(debug){
+                System.err.println("Invalid Size X");
+            }
+            return 0;
+        }
+    }
+    private int getNewSizeY(String sizeY){
+        if(Objects.equals(sizeX, "s")){
+            return frame.getWidth();
+        }else if(sizeY != null && sizeY.matches("[0-9]+")){
+            return Integer.parseInt(sizeY);
+        }else{
+            if(debug){
+                System.err.println("Invalid Size Y");
+            }
+            return 0;
+        }
+    }
+//    private int getAnchor(String anchor){
+//        return switch(anchor){
+//            case"tl" -> 0;
+//            default -> 0;
+//        }
+//    } scale properly or something
     protected String getObjectName() {
-        return "Insert Name Here";
+        return "Shit if fucked up if this gets returned";
     }
 
     private boolean containsNumbers(String str) {
